17e22bc5e7b73cdb16cfbcad5d657382479e0926
Tasnim Rahman <162491945+TASNIM045@users.noreply.github.com>
2024-11-26 00:07:49 +0600

    Update B_BAN_BAN.cpp

--- Weak 5/Day 2/B_BAN_BAN.cpp
+++ Weak 5/Day 2/B_BAN_BAN.cpp
@@ -12,21 +12,36 @@ int main()
         cin >> n;
         string s = "";
         for(int i=0;i<n;i++) {
-            s = s + "BAN";
+            s += "BAN";
         }
-        n = s.size()-1;
-        vector<pair<int,int>> ans;
-        int l = 0,r = n-1;
-        while(l<r) {
-            ans.push_back({l,r});
-            swap(s[l],s[r]);
-            l+=3;
-            r-+3;
+        int cnt = 0;
+        if(s.size()%2==0) {
+            cnt = s.size()/2;
+        } 
+        else {
+            cnt = (s.size()/2)+1;
         }
-        cout<<ans.size()<<'\n';
-        for(auto i : ans) {
-            cout<<i.first<<" "<<i.second<<'\n';
+        vector<int> vo,vt;
+        for(int i=0;i<cnt;i++) {
+            if(s[i]=='A') {
+                vo.push_back(i);
+            }
         }
+        for(int i=s.size()-1;i>=cnt;i--) {
+            if(s[i]=='N') {
+                vt.push_back(i);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<vo.size();i++) {
+            ans.push_back(vo[i]);
+            ans.push_back(vt[i]);
+        }
+        cout<<vo.size()<<'\n';
+        for(auto x : ans) {
+            cout<<x<<" ";
+        }
+        cout<<'\n';
     }
     return 0;
 }
